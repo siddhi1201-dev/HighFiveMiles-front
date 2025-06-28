5b1c5ae3a5cad55994f99e5d4d3d0c874a10216f
Siddhi Pawar <pawarsiddhi0112@gmail.com>
2025-06-28 19:42:36 +0530

    Home page Frontend

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import LoginPage from "./Components/LoginPage";
 import SignupPage from "./Components/SignupPage";
+import HomePage from "./Components/HomePage";
 
 
 function App() {
   return (
       <Router>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
         {/* Add more routes as needed */}
       </Routes>
